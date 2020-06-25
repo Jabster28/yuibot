d614e35114071543947b450bbfe9bed88cbca901
Justyn Boyer <justynboyer@gmail.com>
2020-06-25 14:18:08 +0100

    style: linted src/ due to gts addition

--- src/data.ts
+++ src/data.ts
@@ -1,75 +1,90 @@
-import * as Discord from "discord.js";
-import * as toHex from "colornames";
-
+import * as Discord from 'discord.js';
+import * as toHex from 'colornames';
+declare type command = {
+  run: (
+    msg: Discord.Message,
+    args: string[],
+    client: Discord.Client
+  ) => Promise<unknown>;
+  desc?: string;
+  args?: string;
+};
+// {[key: string]: command}
 const prefix = '-';
-let myID: string = "myIDHere" // TODO: replace with your Snowflake
-let muteRole: string = "mutedRoleIDHere" // TODO: replace with Snowflake of mute role
-let adminRole: string = "mutedRoleIDHere" // TODO: replace with Snowflake of admin role
-export const data = {
+const myID = 'myIDHere'; // TODO: replace with your Snowflake
+const muteRole = 'mutedRoleIDHere'; // TODO: replace with Snowflake of mute role
+const adminRole = 'mutedRoleIDHere'; // TODO: replace with Snowflake of admin role
+export const data: {
+  hiddencommands: {[key: string]: command};
+  commands: {[key: string]: command};
+  prefix: string;
+} = {
   prefix: prefix,
   hiddencommands: {
     coffee: {
       // makes you admin :)
-      run: function (msg: Discord.Message) {
+      run: async function (msg: Discord.Message) {
         console.log(msg.member?.id);
-        return new Promise(function (s) {
-          if (msg.member?.id === myID) {
-            msg.member?.roles.add(adminRole)
-            console.log(msg.guild?.roles);
-            s()
-          }
-        });
-      }
-    }
+        if (msg.member?.id === myID) {
+          msg.member?.roles.add(adminRole);
+          console.log(msg.guild?.roles);
+        }
+      },
+    },
   },
   commands: {
     shh: {
-      desc: "Sleepy time for you. Requires manage roles permission.",
-      args: "(@user)",
-      run: function (msg: Discord.Message, args: Array<string>) {
-        return new Promise(function (res) {
-          let reason;
-          if (!msg.member?.hasPermission("MANAGE_ROLES")) {
-            msg.channel.send("Woah, you're not a mod, dude. Sorry :/");
-          }
-          if (args.length > 1) {
-            reason = ' Reason: <' + args.splice(1).join(" ") + '>';
-          } else {
-            reason = "";
-          }
-          let usr = msg.mentions.members?.first();
-          console.log(usr);
-          usr?.roles.set([muteRole]);
-          let embed = new Discord.MessageEmbed();
-          // @ts-ignore
-          embed.setColor(toHex("mistyrose"));
-          embed.setTitle("_Go to sleep, " + usr?.user.username + "_");
-          embed.setDescription(`${usr?.user.tag} has been put to sleep.${reason}`);
-          embed.setFooter("Bot made by Jabster28#6048");
-          msg.channel.send(embed).then(res)
-        });
-      }
+      desc: 'Sleepy time for you. Requires manage roles permission.',
+      args: '(@user)',
+      run: async function (msg, args) {
+        let reason;
+        if (!msg.member?.hasPermission('MANAGE_ROLES')) {
+          msg.channel.send("Woah, you're not a mod, dude. Sorry :/");
+        }
+        if (args?.length > 1) {
+          reason = ' Reason: <' + args?.splice(1).join(' ') + '>';
+        } else {
+          reason = '';
+        }
+        const usr = msg.mentions.members?.first();
+        console.log(usr);
+        usr?.roles.set([muteRole]);
+        const embed = new Discord.MessageEmbed();
+        embed.setColor(toHex('mistyrose')!);
+        embed.setTitle('_Go to sleep, ' + usr?.user.username + '_');
+        embed.setDescription(
+          `${usr?.user.tag} has been put to sleep.${reason}`
+        );
+        embed.setFooter('Bot made by Jabster28#6048');
+        msg.channel.send(embed);
+      },
     },
     help: {
-      desc: "Shows all commands.",
+      desc: 'Shows all commands.',
       run: function (msg: Discord.Message) {
-        return new Promise(function (res) {
-          let embed = new Discord.MessageEmbed();
-          // @ts-ignore
-          embed.setColor(toHex("orchid"));
-          embed.setTitle("Hi!"); // TODO: replace with bot name
-          embed.setDescription("Hi there! I'm Yui, and I'm here to help you with anything you may need!");
-          let ref: any = data.commands
-          let k: string
-          let ref1
+        return new Promise(res => {
+          const embed = new Discord.MessageEmbed();
+          embed.setColor(toHex('orchid')!);
+          embed.setTitle('Hi!'); // TODO: replace with bot name
+          embed.setDescription(
+            "Hi there! I'm Yui, and I'm here to help you with anything you may need!"
+          );
+          const ref: {[key: string]: command} = data.commands;
+          let k: string;
+          let ref1;
           for (k in ref) {
-            let v = ref[k];
-            embed.addField(prefix + k + " " + ((ref1 = v.args) != null ? ref1 : " "), v.desc);
+            const v = ref[k];
+            embed.addField(
+              prefix + k + ' ' + ((ref1 === v.args) !== undefined ? ref1 : ' '),
+              v.desc
+            );
           }
-          embed.setFooter("Arguments in (parentheses) are required, and arguments in [brackets] are optional and will default to the {braces} option.\nBot made by Jabster28#6048")
+          embed.setFooter(
+            'Arguments in (parentheses) are required, and arguments in [brackets] are optional and will default to the {braces} option.\nBot made by Jabster28#6048'
+          );
           msg.channel.send(embed).then(res);
         });
-      }
-    }
-  }
+      },
+    },
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
 // TODO: change name & others in package.json
-import * as toHex from "colornames"
-import * as Discord from "discord.js"
-import { data } from "./data"
-import * as env from "dotenv"
+import * as toHex from 'colornames';
+import * as Discord from 'discord.js';
+import {data} from './data';
+import * as env from 'dotenv';
 const getArguments = function (x: Discord.Message) {
-  let w = x.content.split(" ")
-  w.shift()
-  return w
+  const w = x.content.split(' ');
+  w.shift();
+  return w;
 };
-env.config()
+env.config();
 
-let client = new Discord.Client();
+const client = new Discord.Client();
 const prefix = data.prefix;
 const commands = data.commands;
 const hiddencommands = data.hiddencommands;
 const token = process.env.TOKEN;
 
-client.on('ready', function () {
-  console.log("Hacking the mainframe with an identity of:");
+client.on('ready', () => {
+  console.log('Hacking the mainframe with an identity of:');
   console.log(client.user?.username);
   console.log("I'm in");
 });
 
-client.on("message", function (msg) {
+client.on('message', msg => {
   const args = getArguments(msg);
-  let v: any, k: string
+  let v: any, k: string;
   for (k in commands) {
     // @ts-ignore
     v = commands[k];
-    if (msg.content.split(" ")[0].toLowerCase() === prefix + k) {
+    if (msg.content.split(' ')[0].toLowerCase() === prefix + k) {
       v.run(msg, args, client);
     }
   }
-  let results = [];
+  const results = [];
   for (k in hiddencommands) {
     // @ts-ignore
     v = hiddencommands[k];
-    if (msg.content.split(" ")[0].toLowerCase() === k) {
+    if (msg.content.split(' ')[0].toLowerCase() === k) {
       results.push(v.run(msg, args, client));
     } else {
       results.push(void 0);
