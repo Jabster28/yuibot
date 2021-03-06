7720733d5bffa27bee0d3dc07cfb384076e938e1
Jabster28 <justynboyer@gmail.com>
2021-03-06 16:36:50 +0000

    refactor: remove useless stuff

--- src/data.ts
+++ src/data.ts
@@ -14,26 +14,16 @@ export const logger = createLogger({
   ),
   defaultMeta: { service: 'yui-bot' },
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-    // - Write all logs error (and below) to `quick-start-error.log`.
-    //
     new transports.File({ filename: 'runtime-errors.log', level: 'error' }),
     new transports.File({ filename: 'runtime.log' }),
   ],
 });
 
-//
-// If we're not in production then **ALSO** log to the `console`
-// with the colorized simple format.
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    })
-  );
-}
+logger.add(
+  new transports.Console({
+    format: format.combine(format.colorize(), format.simple()),
+  })
+);
 
 export type command = {
   run: (
